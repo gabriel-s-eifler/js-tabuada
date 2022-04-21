ccc5bdf791b7afe02fdeb9439684830c73b9e82c
gabriel-s-eifler <101073024+gabriel-s-eifler@users.noreply.github.com>
2022-04-20 22:16:29 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const nume = parseInt(prompt('digite um numero inteiro para ser multiplicado:  '));
+
+for(let um = 1; um <= 10; um++) {
+
+    const result = um * nume;
+     console.log(prompt(result));
+}
